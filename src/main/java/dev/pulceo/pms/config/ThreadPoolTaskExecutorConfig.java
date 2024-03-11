0a8dbb8956a03854d977133d40ed8600f8c9dbb0
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2024-03-11 13:32:35 +0100

    enable asymc

--- src/main/java/dev/pulceo/pms/config/ThreadPoolTaskExecutorConfig.java
+++ src/main/java/dev/pulceo/pms/config/ThreadPoolTaskExecutorConfig.java
@@ -3,12 +3,14 @@ package dev.pulceo.pms.config;
 import dev.pulceo.pms.model.event.PulceoEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 @Configuration
+@EnableAsync
 public class ThreadPoolTaskExecutorConfig {
 
     @Bean
